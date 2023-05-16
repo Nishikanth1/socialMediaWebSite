04c158a82effd41093a8e4651e6b89bf3683612b
Nishikanth23 <35811911+Nishikanth1@users.noreply.github.com>
2023-05-17 02:24:08 +0530

    update server file with new routes

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
-const fastify = require('fastify');
-const { buildapp } = require('./app');
+const fastify = require("fastify");
+const { buildapp } = require("./app");
+
+const userRoutes = require("./routes/users");
+const homeRoutes = require("./routes/routes");
 
 const startApp = async () => {
-  const PORT = process.env.PORT || 3000;
+  const PORT = process.env.PORT || 30000;
   const app = await buildapp();
+
+  app.register(homeRoutes);
+  app.register(userRoutes);
+
   app.listen({ port: PORT }, (err, address) => {
     if (err) {
-      app.logger.error(`Starting serving Failed due ${err}`);
+      app.log.error(`Starting serving Failed due ${err}`);
       process.exit(1);
     }
   });
