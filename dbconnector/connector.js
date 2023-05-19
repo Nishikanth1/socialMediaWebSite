d642a6f637b23668c8855773c6929a461f1d66e2
Nishikanth1 <35811911+Nishikanth1@users.noreply.github.com>
2023-05-20 02:18:42 +0530

    db connect: initial snippet using sequelize orm (#4)
    * try pr
    
    * fix the route plugin
    
    * update lint to use double quote for string
    
    * update server file with new routes
    
    * move docker file to new docker folder
    
    * update npm packages
    
    * check db connectivity in the users api

---
+++ dbconnector/connector.js
@@ -0,0 +1,21 @@
+const { Sequelize } = require("sequelize");
+
+const database = process.env.DATABASE_NAME;
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+const dbUrl = `postgres://${dbUser}:${dbPassword}@localhost:9000/${database}`;// process.env.POSTGRESQL_DB_URI;
+
+const sequelize = new Sequelize(dbUrl, { dialect: "postgres" });
+
+const testDbConnection = async (logger) => {
+  try {
+    await sequelize.authenticate();
+    logger.info("Connection has been established successfully.");
+    return { isDBconnected: true };
+  } catch (error) {
+    logger.error(error, `Unable to connect to the database: ${error}`);
+    return { isDBconnected: false };
+  }
+};
+
+module.exports = { testDbConnection };
