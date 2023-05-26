caf084e399f7cc2c79a027f26c85b3bd88cbefdc
Nishikanth1 <35811911+Nishikanth1@users.noreply.github.com>
2023-05-26 21:48:02 +0530

    update user APIs and also add UTs (#9)
    * install npm pacakages for test- mocha, chai
    
    * update getusers and post user api crud
    
    * move route register to build app
    
    * add package lock to gitignore
    
    * add UTs for get and post apis
    
    * try commiting package lock once

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-const fastify = require('fastify');
+const fastify = require("fastify");
+const userRoutes = require("./modules/users/routes/external/users");
+const homeRoutes = require("./modules/home/routes/external/routes");
 
 const buildapp = async () => {
   const app = fastify({
     logger: true,
   });
+
+  app.register(homeRoutes);
+  app.register(userRoutes);
+
   return app;
 };
 
