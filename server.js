73a694d2c5a37a49a3eefbc783d58ebdff6f7591
Nishikanth23 <35811911+Nishikanth1@users.noreply.github.com>
2023-05-26 21:43:33 +0530

    move route register to build app

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
 const fastify = require("fastify");
 const { buildapp } = require("./app");
 
-const userRoutes = require("./modules/users/routes/external/users");
-const homeRoutes = require("./modules/home/routes/external/routes");
-
 const startApp = async () => {
   const PORT = process.env.PORT || 30000;
   const app = await buildapp();
-
-  app.register(homeRoutes);
-  app.register(userRoutes);
 
   app.listen({ port: PORT }, (err, address) => {
     if (err) {
