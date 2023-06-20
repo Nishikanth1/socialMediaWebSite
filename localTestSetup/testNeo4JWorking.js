f5d0bb5fa07663e61243b4890c615fec4b4e0ead
Nishikanth23 <35811911+Nishikanth1@users.noreply.github.com>
2023-06-20 23:46:53 +0530

    add neo4j container and some helpers, test helper

---
+++ localTestSetup/testNeo4JWorking.js
@@ -0,0 +1,15 @@
+const { runNeo4jQuery } = require("../modules/friends/neo4jHelpers");
+
+const personName = "Alice";
+
+(async function runNeo4jcheck() {
+  const result = await runNeo4jQuery(
+    "MERGE (a:Person {name: $name}) RETURN a",
+    { name: personName },
+  );
+
+  const singleRecord = result.records[0];
+  const node = singleRecord.get(0);
+
+  console.log(node.properties.name);
+}());
