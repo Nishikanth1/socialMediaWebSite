b8a6bd28acbd93770eee8ce378020473c3cab0a6
Nishikanth23 <35811911+Nishikanth1@users.noreply.github.com>
2023-06-21 00:54:35 +0530

    better folder/file names

--- modules/users/routes/external/users.js
+++ modules/users/routes/external/users.js
@@ -1,15 +1,14 @@
-const dbConnector = require("../../../../dbconnector/connector");
-const userController = require("../../controller/user");
+const userService = require("../../service/user");
 const { checkPermissions } = require("../../../middlewares/rbac");
 const { roles } = require("../../../helpers/roles");
 
 async function routes(fastify, options) {
-  fastify.get("/users/:id", { preHandler: checkPermissions([roles.User, roles.Admin]) }, async (request, response) => userController.getUser(request, response, request.log));
-  fastify.put("/users/:id", { preHandler: checkPermissions([roles.User, roles.Admin]) }, async (request, response) => userController.updateUser(request, response, request.log));
-  fastify.patch("/users/:id", { preHandler: checkPermissions([roles.User, roles.Admin]) }, async (request, response) => userController.patchUser(request, response, request.log));
-  fastify.get("/users", { preHandler: checkPermissions([roles.User, roles.Admin]) }, async (request, response) => userController.getAllUsers(request, response, request.log));
-  fastify.post("/users", async (request, response) => userController.addUser(request, response, request.log));
-  fastify.delete("/users/:id", { preHandler: checkPermissions([roles.User, roles.Admin]) }, async (request, response) => userController.deleteUser(request, response, request.log));
+  fastify.get("/users/:id", { preHandler: checkPermissions([roles.User, roles.Admin]) }, async (request, response) => userService.getUser(request, response, request.log));
+  fastify.put("/users/:id", { preHandler: checkPermissions([roles.User, roles.Admin]) }, async (request, response) => userService.updateUser(request, response, request.log));
+  fastify.patch("/users/:id", { preHandler: checkPermissions([roles.User, roles.Admin]) }, async (request, response) => userService.patchUser(request, response, request.log));
+  fastify.get("/users", { preHandler: checkPermissions([roles.User, roles.Admin]) }, async (request, response) => userService.getAllUsers(request, response, request.log));
+  fastify.post("/users", async (request, response) => userService.addUser(request, response, request.log));
+  fastify.delete("/users/:id", { preHandler: checkPermissions([roles.User, roles.Admin]) }, async (request, response) => userService.deleteUser(request, response, request.log));
 }
 
 module.exports = routes;
