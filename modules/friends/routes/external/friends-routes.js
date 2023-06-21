5e3f4221353dceb3832d3fbc927d6eba36875dcf
Nishikanth23 <35811911+Nishikanth1@users.noreply.github.com>
2023-06-22 02:08:28 +0530

    apis for delete usernode in graph, delete follow rel in graph

--- modules/friends/routes/external/friends-routes.js
+++ modules/friends/routes/external/friends-routes.js
@@ -3,8 +3,11 @@ const { roles } = require("../../../helpers/roles");
 const friendsService = require("../../service/friends-service");
 
 async function routes(fastify, options) {
+  fastify.get("/friends/follow/:fromUser/:toUser", async (request, response) => friendsService.getFollow(request, response, request.log));
   fastify.post("/friends/follow", async (request, response) => friendsService.createFollow(request, response, request.log));
+  fastify.delete("/friends/follow/:fromUser/:toUser", async (request, response) => friendsService.deleteFollow(request, response, request.log));
   fastify.post("/friends/user", async (request, response) => friendsService.createUser(request, response, request.log));
+  fastify.delete("/friends/user/:id", async (request, response) => friendsService.deleteUser(request, response, request.log));
 }
 
 module.exports = routes;
