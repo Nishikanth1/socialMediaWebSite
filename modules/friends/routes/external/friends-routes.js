30d3432394caa1b9be018717b9a0d334d5a3e0c5
Nishikanth23 <35811911+Nishikanth1@users.noreply.github.com>
2023-07-09 19:22:30 +0530

    add get all following api

--- modules/friends/routes/external/friends-routes.js
+++ modules/friends/routes/external/friends-routes.js
@@ -4,6 +4,7 @@ const friendsService = require("../../service/friends-service");
 
 async function routes(fastify, options) {
   fastify.get("/friends/follow/:fromUser/:toUser", async (request, response) => friendsService.getFollow(request, response, request.log));
+  fastify.get("/friends/follower/:fromUser", async (request, response) => friendsService.getAllFollowers(request, response, request.log));
   fastify.post("/friends/follow", async (request, response) => friendsService.createFollow(request, response, request.log));
   fastify.delete("/friends/follow/:fromUser/:toUser", async (request, response) => friendsService.deleteFollow(request, response, request.log));
   fastify.post("/friends/user", async (request, response) => friendsService.createUser(request, response, request.log));
