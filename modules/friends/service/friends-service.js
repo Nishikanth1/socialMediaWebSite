ecf1175685021c1183d0a318168b7bb146e4e573
Nishikanth23 <35811911+Nishikanth1@users.noreply.github.com>
2023-07-09 19:22:16 +0530

    fix friends service integer id issue

--- modules/friends/service/friends-service.js
+++ modules/friends/service/friends-service.js
@@ -4,7 +4,7 @@ const neo4jHelpers = require("./neo4jHelpers");
 async function createFollow(request, response, logger) {
   const { fromUserId, toUserId } = request.body;
   const query = `MATCH (from:User {userid: $fromUserId}), (to:User {userid: $toUserId}) 
-                 CREATE (from)-[r:FOLLOWS]->(to)
+                 MERGE (from)-[r:FOLLOWS]->(to)
                  RETURN r`;
   const params = { fromUserId, toUserId };
   const result = await neo4jHelpers.runNeo4jQuery(query, params);
@@ -13,14 +13,16 @@ async function createFollow(request, response, logger) {
   // eslint-disable-next-line dot-notation
   const updatedRels = result.summary.updateStatistics["_stats"].relationshipsCreated;
 
-  if (updatedRels !== 1) {
-    return response.status(500).send({ message: `Unable to create follow from ${fromUserId} to ${toUserId}` });
-  }
+  // if (updatedRels !== 1) {
+  //   return response.status(500).send({ message: `Unable to create follow from ${fromUserId} to ${toUserId}` });
+  // }
   return response.status(200).send({ message, data: { updatedRelationships: updatedRels } });
 }
 
 async function deleteFollow(request, response, logger) {
-  const { fromUserId, toUserId } = request.params;
+  const fromUserId = parseInt(request.params.fromUserId, 10);
+  const toUserId = parseInt(request.params.toUserId, 10);
+
   const query = `MATCH (from:User {userid: $fromUserId})-[r:FOLLOWS]->(to:User {userid: $toUserId}) 
                  DELETE r`;
   const params = { fromUserId, toUserId };
@@ -41,7 +43,7 @@ async function createUser(request, response, logger) {
 }
 
 async function deleteUser(request, response, logger) {
-  const { id } = request.params;
+  const id = parseInt(request.params.id, 10);
   const query = `MATCH (userNode:User {userid: $id})
                  DETACH DELETE userNode`;
   const params = { id };
@@ -50,6 +52,28 @@ async function deleteUser(request, response, logger) {
   return response.status(200).send({ message, data });
 }
 
+function processNeo4jUserNodes(neo4jResult) {
+  const { records } = neo4jResult;
+  // eslint-disable-next-line no-underscore-dangle
+  const nodesData = records.map((node) => node._fields[0].properties);
+  return nodesData;
+}
+
+async function getAllFollowers(request, response, logger) {
+  const fromUser = parseInt(request.params.fromUser, 10);
+  const query = "MATCH (from:User {userid: $fromUserId})-[r:FOLLOWS]->(toNode:User) RETURN (toNode)";
+  logger.info(`getAllFollowers fromUser ${fromUser}`);
+  const params = { fromUserId: fromUser };
+  const result = await neo4jHelpers.runNeo4jQuery(query, params);
+  logger.info(`result is ${JSON.stringify(result)}`);
+  // eslint-disable-next-line dot-notation
+  return response.status(200).send({ data: processNeo4jUserNodes(result) });
+}
+
 module.exports = {
-  createFollow, createUser, deleteUser, deleteFollow,
+  createFollow,
+  createUser,
+  deleteUser,
+  deleteFollow,
+  getAllFollowers,
 };
